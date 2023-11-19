962bcc4d7ef3f3d3a798ee4950cee309f1eecaf8
Abanoub yanni <Abanoubyanni20@gmail.com>
2023-11-19 22:14:27 +0200

    first commit

---
+++ vehicle unit/nrf/nrf_cub_proj/hnd_nrf/Core/Src/Nrf.c
@@ -0,0 +1,18 @@
+/*
+ * Nrf.c
+ *
+ *  Created on: Jun 27, 2023
+ *      Author: LENOVO
+ */
+
+
+#include "stm32f1xx_hal.h"
+
+#define Nrf_CE_port GPIOA
+
+void nrf_writeReg(uint8_t Reg,uint8_t Data)
+{
+
+
+
+}
